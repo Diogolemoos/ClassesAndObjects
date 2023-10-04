a96cb80078603bc654aa55fe76f3d18ae19a8a45
Diogolemoos <146637354+Diogolemoos@users.noreply.github.com>
2023-10-04 13:16:37 +0100

    Added an import statement for the new shapes packages and Triangle class.

---
+++ src/classesandobjects/Person.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+import shapes.Triangle;
+
+/**
+ *
+ * @author diogo
+ */
+public class Person {
+    private String name;    
+    private String nacionality;
+    private String ethnicity;
+    private int age;
+    private double height;
+    private boolean isProgrammer;
+    Triangle favTriangle;//favorite triangle
+    
+}
